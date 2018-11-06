d6819ad5c290c05d5498e3bfea51eec9f10a3b9d
Mathew Welch <gmwva@hotmail.com>
2018-11-05 20:00:21 -0500

    Working through Pet Sim Tutorial

--- Pet Sim/Assets/Scripts/MenuManager.cs
+++ Pet Sim/Assets/Scripts/MenuManager.cs
@@ -7,12 +7,10 @@ public class MenuManager : MonoBehaviour {
 
     public GameObject flashText;
 
-	// Use this for initialization
 	void Start () {
         InvokeRepeating("flashTheText", 0f, 0.5f);
 	}
 	
-	// Update is called once per frame
 	void Update () {
         if (Input.GetMouseButtonUp(0))
             SceneManager.LoadScene("Game");
--- Pet Sim/Assets/Scripts/Robot.cs
+++ Pet Sim/Assets/Scripts/Robot.cs
@@ -11,10 +11,36 @@ public class Robot : MonoBehaviour {
     private int _happiness;
 
     private bool _serverTime;
+    private int _clickCount;
 
 	void Start () {
+        PlayerPrefs.SetString("then", "10/05/2018 12:54:00");
         UpdateStatus();
 	}
+
+    private void Update()
+    {
+        GetComponent<Animator>().SetBool("jump", gameObject.transform.position.y > -2.9f);
+
+        if(Input.GetMouseButtonUp(0))
+        {
+            Vector2 v = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+            RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(v), Vector2.zero);
+            if(hit)
+            {
+                if(hit.transform.gameObject.tag == "robot")
+                {
+                    _clickCount++;
+                    if(_clickCount >= 3)
+                    {
+                        _clickCount = 0;
+                        UpdateHappiness(1);
+                        GetComponent<Rigidbody2D>().AddForce(new Vector2(0, 1000000));
+                    }
+                }
+            }
+        }
+    }
 
     void UpdateStatus()
     {
@@ -36,13 +62,25 @@ public class Robot : MonoBehaviour {
             _happiness = PlayerPrefs.GetInt("_happiness");
         }
 
+        if (!PlayerPrefs.HasKey("then"))
+            PlayerPrefs.SetString("then", GetStringTime());
+
+        TimeSpan ts = GetTimeSpan();
+
+        _hunger -= (int)(ts.TotalHours * 2);
+        if (_hunger < 0)
+            _hunger = 0;
+        _happiness -= (int)((100 - _hunger) * (ts.TotalHours / 5));
+        if (_happiness < 0)
+            _happiness = 0;
+
+        //Debug.Log(GetTimeSpan().ToString());
+        //Debug.Log(GetTimeSpan().TotalHours);
+
         if(_serverTime)
-        {
             UpdateServer();
-        } else
-        {
-            UpdateDevice();
-        }
+        else
+            InvokeRepeating("UpdateDevice", 0f, 30f);
     }
 
     void UpdateServer()
@@ -52,7 +90,21 @@ public class Robot : MonoBehaviour {
 
     void UpdateDevice()
     {
+        PlayerPrefs.SetString("then", GetStringTime());
+    }
 
+    TimeSpan GetTimeSpan()
+    {
+        if (_serverTime)
+            return new TimeSpan();
+        else
+            return DateTime.Now - Convert.ToDateTime(PlayerPrefs.GetString("then"));
+    }
+
+    string GetStringTime()
+    {
+        DateTime now = DateTime.Now;
+        return now.Month + "/" + now.Day + "/" + now.Year + " " + now.Hour + ":" + now.Minute + ":" + now.Second;
     }
 
     public int Hunger
@@ -65,6 +117,13 @@ public class Robot : MonoBehaviour {
     {
         get { return _happiness; }
         set { _happiness = value;  }
+    }
+
+    public void UpdateHappiness(int i)
+    {
+        Happiness += i;
+        if (Happiness > 100)
+            Happiness = 100;
     }
 
 }
